30ee690fbbe9972de19d45624cfc7dc37ea8eb78
Andrius Štikonas <andrius@stikonas.eu>
2017-06-23 21:24:55 +0100

    Bump version to 1.1

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@
 static const char description [] =
     I18N_NOOP ("A game based on Rubik's Cube (TM)");
 
-static const char version [] = "1.0";
+static const char version [] = "1.1";
 
 int main(int argc, char **argv)
 {
